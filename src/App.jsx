d152413c2cf2891baf84363f8f9389ce6146336b
Sayonara <sayonara.crestani@gmail.com>
2023-07-11 11:11:04 +0200

    adds register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Register from './pages/Register';
 
 const App = () => {
-  return <Product/>;
+  return <Register/>;
 };
 
 export default App;
